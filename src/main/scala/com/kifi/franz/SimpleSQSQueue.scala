1a061175a063b0405961a036c80b2f7903786789
Sean Policarpio <sean.policarpio@simplemachines.com.au>
2016-08-11 13:51:28 +1000

    wrapping JSON parsing with Try
    In the event that an SQS queue receives a non-JSON value, JsonSQSQueue
    won't fail with an exception on read and will instead return
    JsUndefined. Consumers of the queue can then safely work with a JsValue
    type.

--- src/main/scala/com/kifi/franz/SimpleSQSQueue.scala
+++ src/main/scala/com/kifi/franz/SimpleSQSQueue.scala
@@ -3,8 +3,9 @@ package com.kifi.franz
 import com.amazonaws.services.sqs.AmazonSQSAsync
 
 import scala.language.implicitConversions
+import play.api.libs.json.{JsValue, Json, JsUndefined}
 
-import play.api.libs.json.{JsValue, Json}
+import scala.util.{Failure, Success, Try}
 
 
 
@@ -15,5 +16,10 @@ class SimpleSQSQueue(protected val sqs: AmazonSQSAsync, val queue: QueueName, pr
 
 class JsonSQSQueue(protected val sqs: AmazonSQSAsync, val queue: QueueName, protected val createIfNotExists: Boolean = false) extends SQSQueue[JsValue] {
   protected implicit def asString(json: JsValue) = Json.stringify(json)
-  protected implicit def fromString(s: String) = Json.parse(s)
+  protected implicit def fromString(s: String) = {
+    Try(Json.parse(s)) match {
+      case Success(json) => json
+      case Failure(e) => JsUndefined(e.getMessage)
+    }
+  }
 }
